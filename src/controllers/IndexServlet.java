55ab895742b59615d7d9a62b905c7990172cd2e6
masamichi.kawakami <gate.of.steiner.main.theme@gmail.com>
2022-06-25 16:45:59 +0900

    getAlltaskを追加
    Message.java→task.javaに変更

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,26 +1,33 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Task;
+import utils.DBUtil;
+
+
+
 /**
- * Servlet implementation class index
+ * Servlet implementation class IndexServlet
  */
 @WebServlet("/index")
-public class index extends HttpServlet {
+public class IndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public index() {
+    public IndexServlet() {
         super();
-
+        // TODO Auto-generated constructor stub
     }
 
     /**
@@ -28,7 +35,12 @@ public class index extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        EntityManager em = DBUtil.createEntityManager();
+
+        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
+        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
+
+        em.close();
     }
 
 }
--- src/models/Task.java
+++ src/models/Task.java
@@ -1,6 +1,5 @@
 package models;
 
-
 import java.sql.Timestamp;
 
 import javax.persistence.Column;
@@ -8,58 +7,49 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
-
-
-
 @Entity
-@Table(name = "messages")
-
-public class message {
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllTasks",
+            query = "SELECT m FROM Task AS m ORDER BY m.ID DESC"
+            )
+})
+@Table(name = "tasks")
+public class Task {
     @Id
     @Column(name = "ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
+    private Integer ID;
     @Column(name = "作成日時", nullable = false)
     private Timestamp created_at;
-
     @Column(name = "更新日時", nullable = false)
     private Timestamp updated_at;
-
-
     @Column(name = "content", length = 255, nullable = false)
     private String content;
-
-
     public Integer getId() {
-        return id;
+        return ID;
     }
-
-    public void setId(Integer id) {
-        this.id = id;
+    public void setId(Integer ID) {
+        this.ID = ID;
     }
-
     public Timestamp getCreated_at() {
         return created_at;
     }
-
     public void setCreated_at(Timestamp created_at) {
         this.created_at = created_at;
     }
-
     public Timestamp getUpdated_at() {
         return updated_at;
     }
-
     public void setUpdated_at(Timestamp updated_at) {
         this.updated_at = updated_at;
     }
-
     public String getContent() {
         return content;
     }
-
     public void setContent(String content) {
         this.content = content;
     }
